682967667e9c8bc67497076451b5572d74df7bb3
Jenn Nelson <jnelson@edenpr.org>
2017-03-13 18:17:35 -0500

    initial files

---
+++ src/arrayList/ImportingFiles.java
@@ -0,0 +1,39 @@
+package arrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ImportingFiles {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//create an array list of objects that match the type of items
+		//found in your file that you plan to import
+		ArrayList<String> allWords = new ArrayList<String>();
+		//scan in the words, one on each line
+		Scanner input = new Scanner(new File("words.txt"));
+		while (input.hasNext()) {
+		    String word = input.next();
+		    allWords.add(word);
+		}
+		System.out.println(allWords);
+
+		//move each of the plural words to another file
+		
+		// remove all plural words
+		for (int i = 0; i < allWords.size(); i++) {
+		    String word = allWords.get(i);
+		    if (word.endsWith("s")) {
+		        allWords.remove(i);
+		        i--;
+		    }
+		}
+		
+		//filter out the bad words, removing them if they contain parts of
+		//other bad words. Create another list of the bad words, and loop over them,
+		//instead of looking for each one. This way you can add to the bad word list
+		//over time.
+	}
+
+}
